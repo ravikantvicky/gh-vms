008a51b5b4b9c86f0801e6759ca8e79c1dfef912
Ravi Kant Pandey <ravikantvicky@gmail.com>
2019-04-06 02:33:12 +0530

    cors

--- src/main/java/com/global/vms/GhVmsApplication.java
+++ src/main/java/com/global/vms/GhVmsApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @PropertySources({ @PropertySource("classpath:vms.properties"), @PropertySource("classpath:db.properties"),
@@ -27,5 +28,14 @@ public class GhVmsApplication {
 		executor.setMaxPoolSize(10);
 		executor.setQueueCapacity(25);
 		return executor;
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		return loggingFilter;
 	}
 }
--- src/main/java/com/global/vms/config/VMSInterceptor.java
+++ src/main/java/com/global/vms/config/VMSInterceptor.java
@@ -1,8 +1,14 @@
 package com.global.vms.config;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
@@ -16,7 +22,7 @@ import com.global.vms.util.WebUtils;
 
 @Component
 public class VMSInterceptor implements HandlerInterceptor {
-
+	private final Logger log = LoggerFactory.getLogger(VMSInterceptor.class);
 	@Autowired
 	Environment env;
 
@@ -26,6 +32,13 @@ public class VMSInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		Map<String, String> reqheader = new HashMap<>();
+		Enumeration<String> allHeaders = request.getHeaderNames();
+		while (allHeaders.hasMoreElements()) {
+			String headerName = allHeaders.nextElement();
+			reqheader.put(headerName, request.getHeader(headerName));
+		}
+		log.info("Request Header: {}", reqheader);
 		String token = request.getHeader(env.getProperty("key.header.token", "access-token"));
 		if (token == null || !tokenUtils.verifyToken(token, WebUtils.getIp(request))) {
 			ServiceResponse<Object> unauthorizedResponse = new ServiceResponse<>(
@@ -41,6 +54,13 @@ public class VMSInterceptor implements HandlerInterceptor {
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+		reqheader = new HashMap<>();
+		allHeaders = request.getHeaderNames();
+		while (allHeaders.hasMoreElements()) {
+			String headerName = allHeaders.nextElement();
+			reqheader.put(headerName, request.getHeader(headerName));
+		}
+		log.info("Request Header at end of interceptor: {}", reqheader);
 		return HandlerInterceptor.super.preHandle(request, response, handler);
 	}
 }

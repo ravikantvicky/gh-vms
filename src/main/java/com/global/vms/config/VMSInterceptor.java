c894dfbcf93b85e7365ab4e1246b632a6ed03104
Ravi Kant Pandey <ravikantvicky@gmail.com>
2019-04-06 01:53:06 +0530

    cors

--- src/main/java/com/global/vms/config/VMSInterceptor.java
+++ src/main/java/com/global/vms/config/VMSInterceptor.java
@@ -36,7 +36,7 @@ public class VMSInterceptor implements HandlerInterceptor {
 			response.getWriter().write(new ObjectMapper().writeValueAsString(unauthorizedResponse));
 			return false;
 		}
-		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
